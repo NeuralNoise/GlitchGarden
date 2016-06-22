376a1a7c9b86574440ed582013d3e1ec7563daf3
Adrien Gannerie <adrien.gannerie@gmail.com>
2016-06-23 01:03:25 +0200

    Création du projet et travail du 22/06/2016

---
+++ Assets/Scripts/General/MusicPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    static MusicPlayer instance = null;
+
+    public AudioClip[] clips;
+
+    private AudioSource music;
+
+    void Awake()
+    {
+        music = GetComponent<AudioSource>();
+        if (instance != null && instance != this)
+        {
+            Debug.Log("Destroy :" + this.GetHashCode().ToString());
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            OnLevelWasLoaded(0);
+        }
+    }
+    
+    void OnLevelWasLoaded(int level)
+    {
+        if(music && music.isActiveAndEnabled)
+        {
+            music.Stop();
+            try
+            {
+                music.clip = clips[level];
+                music.Play();
+            }
+            catch
+            {
+                Debug.Log("No music to load.");
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/General/SplashScreenManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplashScreenManager : MonoBehaviour
+{
+
+    public float duration = 3f;
+    public AudioClip sound;
+    // Use this for initialization
+    void Start()
+    {
+        AudioSource.PlayClipAtPoint(sound, transform.position);
+        FindObjectOfType<LevelManager>().Invoke("LoadNextLevel", duration);
+    }
+}
